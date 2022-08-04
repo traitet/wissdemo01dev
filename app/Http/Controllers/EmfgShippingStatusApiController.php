5f7096af8034e6eb3462eee22963084e891f8eb1
gitsuchart <suchart_au@aisin-ap.com>
2022-08-04 12:17:29 +0700

    shipping status api

--- app/Http/Controllers/EmfgShippingStatusApiController.php
+++ app/Http/Controllers/EmfgShippingStatusApiController.php
@@ -21,14 +21,14 @@ use App\Models\UserPermission;
 class EmfgShippingStatusApiController extends Controller
 {
 
-// ==========================================================================
-// DECLARE END POINT
-// ==========================================================================
+    // ==========================================================================
+    // DECLARE END POINT
+    // ==========================================================================
     private $ENDPOINT = 'http://10.40.0.4:8080/wissdemo01/public/api/emfg_shipping_order_status_obj';
 
-// ==========================================================================
-// GET DATA
-// ==========================================================================
+    // ==========================================================================
+    // GET DATA
+    // ==========================================================================
     function getData(Request $req)
     {
         $this->validate($req, [
@@ -42,52 +42,52 @@ class EmfgShippingStatusApiController extends Controller
         // ==========================================================================
         $api = '';
         // ======================================================================
-    // SET DATA WRITE LOG
-    // ======================================================================
-    $permissionName = $req->permissionAuth;
-    $permissionID = UserPermission::getPermissionID($permissionName);
-    $optionValue = $req->input('docNum')??'empty';
-// ======================================================================
+        // SET DATA WRITE LOG
+        // ======================================================================
+        $permissionName = $req->permissionAuth;
+        $permissionID = UserPermission::getPermissionID($permissionName);
+        $optionValue = $req->input('docNum') ?? 'empty';
+        // ======================================================================
         // ======================================================================
         // SET DATA RETURN TO VIEW
         // ======================================================================
-            $docNumRtv = $req->input('docNum');
-            $dateStartRtv = $req->input('dateStart');
-            $dateEndRtv = $req->input('dateEnd');
-            $maxRecordRtv = $req->input('maxRecord');
+        $docNumRtv = $req->input('docNum');
+        $dateStartRtv = $req->input('dateStart');
+        $dateEndRtv = $req->input('dateEnd');
+        $maxRecordRtv = $req->input('maxRecord');
         // ==========================================================================
         // CHECK INPUT IF NOT EMPTY
         // ==========================================================================
-            // ======================================================================
-            // GET DATA
-            // ======================================================================
-            $dateStart = str_replace('-','',$req->input('dateStart')??'20220101');
-            $dateEnd = str_replace('-','',$req->input('dateEnd')??'20220101');
-            $maxRecord = $req->input('maxRecord')??'10';
-            $docNum = $req->input('docNum')??'';
-            $queryStr = "start_date=$dateStart&end_date=$dateEnd&doc_num=$docNum&max_record=$maxRecord";
+        // ======================================================================
+        // GET DATA
+        // ======================================================================
+        $dateStart = str_replace('-', '', $req->input('dateStart') ?? '20220101');
+        $dateEnd = str_replace('-', '', $req->input('dateEnd') ?? '20220101');
+        $maxRecord = $req->input('maxRecord') ?? '10';
+        $docNum = $req->input('docNum') ?? '';
+        $queryStr = "start_date=$dateStart&end_date=$dateEnd&doc_num=$docNum&max_record=$maxRecord";
 
-            // ======================================================================
-            // CALL API
-            // ======================================================================
-            $url = $this->ENDPOINT . $api ."/". $queryStr;
-            $response = Http::get($url);
-            error_log($url);
-            // ======================================================================
-            // IF CALL SUCCCESS
-            // ======================================================================
-            if ($response->status() == 200) {
-                $result = json_decode($response->body(), true);
-                if(!empty($result)){
-                    $keyArray = array_keys($result[0]);
-                    Log::insertLog(Auth::user()->id, $permissionID,'Search '.$permissionName.' '.$optionValue.' completed');
-                    return view('emfg-shipping-status', compact('result', 'keyArray','docNumRtv','dateStartRtv','dateEndRtv','maxRecordRtv','permissionName'));
-                }else{
-                    //need to return no data msg
-                    $keyArray = [];
-                }
+        // ======================================================================
+        // CALL API
+        // ======================================================================
+        $url = $this->ENDPOINT . $api . "/" . $queryStr;
+        $response = Http::get($url);
+        error_log($url);
+        // ======================================================================
+        // IF CALL SUCCCESS
+        // ======================================================================
+        if ($response->status() == 200) {
+            $result = json_decode($response->body(), true);
+            if (!empty($result)) {
+                $keyArray = array_keys($result[0]);
+                Log::insertLog(Auth::user()->id, $permissionID, 'Search ' . $permissionName . ' ' . $optionValue . ' completed');
+                return view('emfg-shipping-status', compact('result', 'keyArray', 'docNumRtv', 'dateStartRtv', 'dateEndRtv', 'maxRecordRtv', 'permissionName'));
+            } else {
+                //need to return no data msg
+                $keyArray = [];
             }
-            Log::insertLog(Auth::user()->id, $permissionID,'Search '.$permissionName.' '.$optionValue.' not found');
-            return view('emfg-shipping-status',compact('result', 'keyArray','docNumRtv','dateStartRtv','dateEndRtv','maxRecordRtv','permissionName'));
+        }
+        Log::insertLog(Auth::user()->id, $permissionID, 'Search ' . $permissionName . ' ' . $optionValue . ' not found');
+        return view('emfg-shipping-status', compact('result', 'keyArray', 'docNumRtv', 'dateStartRtv', 'dateEndRtv', 'maxRecordRtv', 'permissionName'));
     }
 }
