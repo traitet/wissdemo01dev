bf6a7443707f617685ca6d89addc3e7e4a188e72
gitsuchart <suchart_au@aisin-ap.com>
2022-08-04 12:27:35 +0700

    test API 04082022

--- resources/views/deploy-code.blade.php
+++ resources/views/deploy-code.blade.php
@@ -83,7 +83,7 @@
                     <div class="container-fluid">
                         <div class="d-sm-flex align-items-center justify-content-between mb-4">
 
-                            <h1 class="h3 mb-0 text-gray-800">Git pull - Deploy wissdemo01 from GitHub main branch </h1>
+                            <h1 class="h3 mb-0 text-gray-800">Git pull - Deploy wissdemodev01 from GitHub main branch </h1>
                         </div>
                         <div class="row">
                             <div class="col-xl-12 col-lg-12">
