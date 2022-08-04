77ca0b65e5e616f736619fbb67c5a934252599b3
gitsuchart <suchart_au@aisin-ap.com>
2022-08-04 09:02:51 +0700

    user manual

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -364,14 +364,12 @@
                         <div class="card-body">
                             <div class="text-center">
                                 <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                    src="img/undraw_posting_photo.svg" alt="...">
+                                src="{{ asset('theme/img/undraw_posting_photo.svg') }}" alt="...">
                             </div>
-                            <p>Add some quality, svg illustrations to your project courtesy of <a target="_blank"
-                                    rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                                constantly updated collection of beautiful svg images that you can use
-                                completely free and without attribution!</p>
-                            <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                                unDraw &rarr;</a>
+                            <p>Good day every one,</p>
+                                <p class="mb-0">Welcome to Web IT Self Service of Aisin Group. Click in this link for <a target="_blank"
+                                    rel="nofollow" href="assets/User Manual WISS R1.pdf" >WISS User Manual</a>, When you
+                                have question and concerns point please infrom to us for improvement.</p>
                         </div>
                     </div>
 
@@ -381,11 +379,12 @@
                             <h6 class="m-0 font-weight-bold text-primary">WISS SDR</h6>
                         </div>
                         <div class="card-body">
-                            <p>SB Admin 2 makes extensive use of Bootstrap 4 utility classes in order to reduce
-                                CSS bloat and poor page performance. Custom CSS classes are used to create
-                                custom components and custom utility classes.</p>
-                            <p class="mb-0">Before working with this theme, you should become familiar with the
-                                Bootstrap framework, especially the utility classes.</p>
+                            <div class="text-center">
+                                <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
+                                src="{{ asset('theme/img/WISS concept.png') }}" alt="...">
+                            </div>
+                            <p>System Overview</p>
+                            <p class="mb-0">In finally working Web IT Self Service, Integrate and support many system in Aisin Group</p>
                         </div>
                     </div>
 
