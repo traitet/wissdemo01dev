ff608119809a6fb3b64f9919780a75e8e81fa3d6
gitsuchart <suchart_au@aisin-ap.com>
2022-08-09 12:01:53 +0700

    icon

--- resources/views/theme/sidebar.blade.php
+++ resources/views/theme/sidebar.blade.php
@@ -1,4 +1,5 @@
-       <!-- Sidebar -->
+
+<!-- Sidebar -->
        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
            <!-- Sidebar - Brand -->
